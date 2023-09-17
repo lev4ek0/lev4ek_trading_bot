c97bf26d4967490e5a525c3db709bf3cb1dbdfe0
lev <leva22.08.01@inbox.ru>
2023-09-17 21:23:36 +0300

    s

--- bot/handlers/common.py
+++ bot/handlers/common.py
@@ -7,6 +7,6 @@ def create_router():
     router.message.middleware(SessionMiddleware())
     router.callback_query.middleware(SessionMiddleware())
     router.message.middleware(UserMiddleware())
-    router.message.middleware(MetricsMiddleware())
     router.message.middleware(ErrorsMiddleware())
+    router.message.middleware(MetricsMiddleware())
     return router
