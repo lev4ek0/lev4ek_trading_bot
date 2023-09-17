0f69dfb2c6d12fe4af633d8246b9601df7145c55
lev <leva22.08.01@inbox.ru>
2023-09-17 21:32:35 +0300

    fix

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -21,5 +21,4 @@ async def cmd_start(message: types.Message):
         "/my_specialities\n"
         "/remove_speciality\n"
     )
-    raise KeyError("key error")
     await message.answer(text=text)
--- bot/middleware/metrics.py
+++ bot/middleware/metrics.py
@@ -1,12 +1,13 @@
+from datetime import datetime
 from typing import Any, Awaitable, Callable, Dict
 
 from aiogram import BaseMiddleware
 from aiogram.types import Message
-from prometheus_client import Counter
+from prometheus_client import Counter, Summary
 
 ok_rps = Counter('ok_rps', 'Ok rps counter')
 bad_rps = Counter('bad_rps', 'Bad rps counter')
-
+request_latency_seconds = Summary('request_latency_seconds', 'Request latency, seconds')
 
 class MetricsMiddleware(BaseMiddleware):
     async def __call__(
@@ -16,7 +17,9 @@ class MetricsMiddleware(BaseMiddleware):
         data: Dict[str, Any],
     ) -> Any:
         try:
+            now = datetime.now()
             result = await handler(event, data)
+            request_latency_seconds.observe((datetime.now() - now).total_seconds())
             ok_rps.inc()
         except Exception as e:
             bad_rps.inc()
